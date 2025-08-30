616f0de8b7dae6460135c682e8333f4aac34f8af
victorceronl <ceronloezavictor@gmail.com>
2025-08-30 14:46:52 -0600

    Intento de integración de templates. Es mejor modificar el cogido Py2

--- tiendaEbooks/downloadButtons/admin.py
+++ tiendaEbooks/downloadButtons/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import File, Download
 
-# Register your models here.
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ("name", "file", "download_count")
+
+@admin.register(Download)
+class DownloadAdmin(admin.ModelAdmin):
+    list_display = ("file", "downloaded_at")
